de0cbfd93a794c7b6ee84ba156f8d5fe71a7f3b2
jyssys <jyssys@naver.com>
2023-09-03 17:09:25 +0900

    list algorithm

--- list/Two_sum.py
+++ list/Two_sum.py
@@ -1,11 +1,27 @@
-def twoSum(nums, target):
+# 시간복잡도 O(n^2) 알고리즘 (완전탐색)
+
+def twoSum1(nums, target):
     n = len(nums)
-    
+
     for i in range(0, n):
         for j in range(i+1, n):
             if nums[i] + nums[j] == target:
                 return True
-        
+
     return False
 
-print(twoSum(nums = [4,1,9,7,5,3,16], target = 14))
+# 시간복잡도 O(nlog(n)) 알고리즘 (정렬 후 탐색)
+
+def twoSum2(nums, target):
+    # O(nlog(n))
+    nums.sort()
+    l, r = 0, len(nums)-1
+    
+    # O(n)
+    while l<r:
+        if nums[l] + nums[r] == target: return True
+        elif nums[l] + nums[r] > target: r -= 1
+        elif nums[l] + nums[r] < target: l += 1
+    return False
+
+print(twoSum1(nums=[4, 1, 9, 7, 5, 3, 16], target=14))
