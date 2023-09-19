fb73240cd04f3081778040d5ffe7b66edd2688a3
jyssys <jyssys@naver.com>
2023-09-19 17:41:22 +0900

    graph opt

--- graph/graph.py
+++ graph/graph.py
@@ -26,15 +26,31 @@ class graphDFS(object):
         self.graph = graph
         self.visited = []
     
+    def init_visited(self):
+        self.visited = []
+    
     def dfs(self, start_v):
         self.visited.append(start_v)
         
         for v in self.graph[start_v]:
             if v not in self.visited:
                 self.dfs(v)
+                
+    def bfs(self, start_v):
+        self.visited.append(start_v)
+        queue = deque(start_v)
+        
+        while queue:
+            cur_v = queue.popleft()
+            for v in self.graph[cur_v]:
+                if v not in self.visited:
+                    self.visited.append(v)
+                    queue.append(v)
+        
     
 
 # print(bfs(graph, 'A'))
 new_graph = graphDFS(graph)
-new_graph.dfs('A')
+# new_graph.dfs('A')
+new_graph.bfs('A')
 print(new_graph.visited)
