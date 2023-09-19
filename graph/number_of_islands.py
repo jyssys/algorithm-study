97c5ab6ea6a8770d76ba1f65fd033c647c97ed8a
jyssys <jyssys@naver.com>
2023-09-19 21:26:23 +0900

    number of islands

---
+++ graph/number_of_islands.py
@@ -0,0 +1,54 @@
+from collections import deque
+
+island_1 = [
+    ['1', '1','0','0','0'],
+    ['1', '1','0','0','0'],
+    ['0', '0','1','0','0'],
+    ['0', '0','0','1','1']
+]
+
+class graph(object):
+    def __init__(self, graph = None):
+        self.graph = graph
+        self.visited = []
+        
+    def init_visited(self):
+        self.visited = []
+        
+    def numIslandsBFS(self):
+        number_of_islands = 0
+        
+        m = len(self.graph)
+        n = len(self.graph[0])
+        
+        self.visited = [[False]*n for _ in range(m)]
+        
+        def bfs(x, y):
+            dx = [-1, 1, 0, 0] # 상 하 좌 우
+            dy = [0, 0, -1, 1]
+            
+            self.visited[x][y] = True
+            queue = deque()
+            queue.append((x, y))
+            
+            while queue:
+                cur_x, cur_y = queue.popleft()
+                for i in range(4):
+                    next_x = cur_x + dx[i]
+                    next_y = cur_y + dy[i]
+                    
+                    if next_x >= 0 and next_x < m and next_y >= 0 and next_y < n: # 벽이나 모서리에서는 방문이 불가할 수 있음.
+                        if self.graph[next_x][next_y] == '1' and not self.visited[next_x][next_y]: # 방문하면 안되는 좌표
+                            self.visited[next_x][next_y] = True
+                            queue.append((next_x, next_y))
+        
+        for i in range(m):
+            for j in range(n):
+                if self.graph[i][j] == '1' and not self.visited[i][j]:
+                    bfs(i, j)
+                    number_of_islands += 1
+                        
+        return number_of_islands
+    
+new_graph = graph(island_1)
+print(new_graph.numIslandsBFS())
